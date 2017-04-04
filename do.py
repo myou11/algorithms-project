9e528636c61b0aa6451a9e7b6bc586013af79d96
Cole Frederick <u0xee@rams.colostate.edu>
2017-04-04 09:07:14 -0600

    Add comments for code structure in do.py

--- do.py
+++ do.py
@@ -21,16 +21,22 @@ def start_ipython(args):
     command = ["ipython3"]
     subprocess.call(command, stdout=sys.stdout, stdin=sys.stdin, stderr=sys.stderr)
 
+###
+### Main parser
 parser = argparse.ArgumentParser(description='Scaffolding to support project development.')
 parser.set_defaults(func=lambda args: parser.print_usage())
 subparsers = parser.add_subparsers(help='action to perform')
 
+
+## Test subparser
 test_parser = subparsers.add_parser('test', aliases=['tests'],
                                         description='Perform unit tests',
                                         help='Perform unit tests (-v for verbose)')
 test_parser.set_defaults(func=run_tests)
 test_parser.add_argument('-v', '--verbose', action='store_true')
 
+
+## Tar subparser
 tar_parser = subparsers.add_parser('tar', description='Package project up',
                                        help='Package project up (-h for options)')
 tar_parser.set_defaults(func=create_tarball)
@@ -39,13 +45,14 @@ tar_parser.add_argument('-f', '--filename',
                             default=default_tar_filename,
                             help='name for the output tar file')
 
+
+## REPL subparser
 repl_parser = subparsers.add_parser('repl', description='Start an interactive python session',
                                         help='Start an interactive python session')
 repl_parser.set_defaults(func=start_ipython)
 
+
+###
+### Parse and dispatch
 args = parser.parse_args()
 args.func(args)
-
-
-
-
