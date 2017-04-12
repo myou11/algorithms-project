ee81cec8a924198adf472c5dfc62761a7fd26aaf
Maxwell You <myou@lincoln.cs.colostate.edu>
2017-04-11 21:35:12 -0600

    finished dicts.py

--- builtin_types/dicts.py
+++ builtin_types/dicts.py
@@ -2,7 +2,19 @@ def dict_length(D):
     return len(D)
 
 def sorted_list_of_items(D):
-    return sorted(list(D.items))
+    return sorted(list(D.items()))
 
 def dict_from_keys_values(k, v):
     return dict(zip(k,v))
+
+def inverted_dict(D):
+    return {v:k for k, v in D.items()}
+
+def update_key(D, k, v):
+    D[k] = v
+
+def sorted_list_of_keys(D):
+    return sorted(D.keys())
+
+def remove_key(D, k):
+    del D[k]
